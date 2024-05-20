81fa36b2fd987e744e3489555da1cac557cd3ad1
Marcus <sajlipe@live.com>
2024-05-20 17:42:38 -0300

    java

---
+++ script.js
@@ -0,0 +1,85 @@
+const db = {
+    cidade: [
+        {
+            cidade1: "São Paulo",
+            quantidade: 15,
+            classe: ["econômica", "executiva", "primeira"],
+            passagem: ["ida", "volta"]
+
+        },
+        {
+            cidade2: "Rio de Janeiro",
+            quantidade: 12,
+            classe: ["econômica", "executiva"],
+            passagem: ["ida"]
+
+        },
+        {
+            cidade3: "Belo Horizonte",
+            quantidade: 5,
+            classe: ["executiva", "primeira"],
+            passagem: ["ida", "volta"]
+
+        },
+        {
+            cidade4: "Salvador",
+            quantidade: 9,
+            classe: ["econômica", "executiva", "primeira"],
+            passagem: ["ida", "volta"]
+
+        },
+        {
+            cidade5: "Brasília",
+            quantidade: 19,
+            classe: ["primeira"],
+            passagem: ["ida", "volta"]
+
+        },
+        {
+            cidade6: "Curitiba",
+            quantidade: 30,
+            classe: ["econômica", "executiva", "primeira"],
+            passagem: ["ida", "volta"]
+
+        },
+        {
+            cidade7: "Fortaleza",
+            quantidade: 10,
+            classe: ["econômica", "primeira"],
+            passagem: ["volta"]
+
+        },
+        {
+            cidade8: "Recife",
+            quantidade: 50,
+            classe: ["econômica"],
+            passagem: ["volta"]
+
+        },
+        {
+            cidade9: "Porto Alegre",
+            quantidade: 9,
+            classe: ["econômica", "executiva", "primeira"],
+            passagem: null
+        },
+        {
+            cidade10: "Manaus",
+            quantidade: null,
+            classe: null,
+            passagem: ["ida", "volta"]
+
+        },
+        {
+            cidade11: "Capela do alto alegre",
+            quantidade: 5,
+            classe: ["econômica", "executiva"],
+            passagem: ["ida", "volta"],
+        },
+        {
+            cidade12: "santo antônio de jesus",
+            quantidade: 30,
+            classe: ["executiva"],
+            passagem: ["ida", "volta"],
+        }
+    ]   
+}
